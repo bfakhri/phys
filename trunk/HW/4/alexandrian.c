76ba2aa5927201508e1178d4203cfb3cb2250013
bijan@umich.edu <bijan@umich.edu@65d18b45-c968-b410-25b9-1799288fbc43>
2014-11-11 09:04:36 +0000

    Added functions to access work buffer

--- trunk/HW/4/alexandrian.c
+++ trunk/HW/4/alexandrian.c
@@ -1,6 +1,6 @@
 // Tentative questions: 
 // In email, when describing f(x), the 3rd line is "z = x". Why isn't it z = 0? 
-//
+// Try setting number of threads to 1.5X number of procs to do hyperthreading? 
 
 #include <stdio.h>
 #include <math.h>
@@ -11,6 +11,7 @@
 #define END_B 100
 #define epsilon 0.000001 // 10^-6
 #define slope 12
+#define WORK_BUFF_SIZE 100
 
 using namespace std; 
 
@@ -34,16 +35,54 @@ double f(double x)
 
 
 double GLOB_MAX = 0; 
+double GLOB_C_Arr[WORK_BUFF_SIZE];
+double GLOB_D_Arr[WORK_BUFF_SIZE];
+bool GLOB_BuffFull = false;	// Signals whether work buffer is full
+
+// Maybe hide these variable better? 
+int buffHead = 0; 
+int buffTail = 1; 
+
+// These buffer manipulation functions
+// should only be allowed to be called 
+// on thread at a time!
+// Check to make sure negatives work for the head/tail with modulo
+void bufferPush(double c, double d)
+{
+	GLOB_C_Arr[buffTail] = c; 
+	GLOB_D_Arr[buffTail] = d; 
+	buffTail = (buffTail+1)%WORK_BUFF_SIZE; 
+	if(buffHead == buffTail)
+	{
+		// Buffer is now full
+		GLOB_BuffFull = true; 
+	}
+}
+
+void bufferPop(double* c, double* d)
+{
+	*c = GLOB_C_Arr[buffHead];
+	*d = GLOB_D_Arr[buffHead];
+	buffHead = (buffHead-1)%WORK_BUFF_SIZE; 
+
+	// THIS IS NOT THE RIGHT INDICATOR FOR EMPTINESS
+	if(buffHead == buffTail)
+	{
+		// Buffer is now empty
+		GLOB_BuffFull = false; 
+	}
+}
+
 
 int main(int argc, char *argv[]) 
 {
+	omp_set_num_threads(omp_get_num_procs()); 
 	double input = 0; 
 	input = atoi(argv[1]); 
 	// This is just to test f
 	printf("f(%2.0f) = %1.13f\n\n", input, f(input)); 
 	printf("Num Procs: %d\n\n", omp_get_num_procs()); 
+	printf("My ThreadNum: %d\n\n", omp_get_thread_num()); 
 	
-	omp_set_num_threads(omp_get_num_procs()); 
-	omp_get_thread_num(); 
 	return 0; 
 }

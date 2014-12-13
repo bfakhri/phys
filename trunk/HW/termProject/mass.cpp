d17c2753185691ea014aee8c3b3aa2c8f70c4895
bijan@umich.edu <bijan@umich.edu@65d18b45-c968-b410-25b9-1799288fbc43>
2014-12-13 12:06:38 +0000

    I believe it is working, now we just need bignum libraries to represent real celestial values

--- trunk/HW/termProject/mass.cpp
+++ trunk/HW/termProject/mass.cpp
@@ -1,6 +1,6 @@
 #include "mass.h"
 
-mass::mass()
+Mass::Mass()
 {
 	objectMass = 1; 	// Default 1Kg
 	position.x = 0; 
@@ -17,47 +17,67 @@ mass::mass()
 		
 }
 
-double mass::getMass(){
+double Mass::getMass(){
 	return objectMass; 
 }
 
-cartesian mass::getPos(){
+cartesian Mass::getPos(){
 	return position; 
 }
 
-cartesian mass::getVelocity(){
+cartesian Mass::getVelocity(){
 	return velocity; 
 }
 
-void mass::setMass(double newMass){
+void Mass::setMass(double newMass){
 	objectMass = newMass; 
 }
 
-void mass::setPos(cartesian newPos){
+void Mass::setPos(cartesian newPos){
 	position = newPos;
 }
 
-void mass::setVelocity(cartesian newVelocity){
+void Mass::setVelocity(cartesian newVelocity){
 	velocity = newVelocity; 
 }
 
-void mass::resetForces(){
+void Mass::resetForces(){
 	cumalForces.x = 0; 
 	cumalForces.y = 0; 
 	cumalForces.z = 0; 
 }
 
-void mass::addForce(cartesian force){
+void Mass::addForce(cartesian force){
 	cumalForces.x += force.x; 
 	cumalForces.y += force.y; 
 	cumalForces.z += force.z;
 }	
 
+double Mass::newtonGrav(double objMass, double distance){
+	return G*(objectMass*objMass)/(distance*distance); 
+}
+
+void Mass::influence(Mass obj){
+	cartesian objPos = obj.getPos();
+	cartesian diffPos; 
+	diffPos.x = position.x - objPos.x;
+	diffPos.y = position.y - objPos.y;
+	diffPos.z = position.z - objPos.z;
+
+	double distance = sqrt(diffPos.x*diffPos.x + diffPos.y * diffPos.y + diffPos.z * diffPos.z); 
+
+	double netForce = newtonGrav(obj.getMass(), distance); 
+
+	// Check this math just in case
+	cumalForces.x += netForce * diffPos.x/distance; 
+	cumalForces.y += netForce * diffPos.y/distance; 
+	cumalForces.z += netForce * diffPos.z/distance; 	
+}
 // MAKE FASTER BY GETTING RID OF REDUNDANT COMPUTING
 // F = ma, a = F/m
 // v = v0 + at  ---> v = v0 + F/m * t
 // d = d0 + v0*t + 0.5*a*t^2
-cartesian mass::updateVelAndPos(double timeStep){
+cartesian Mass::updateVelAndPos(double timeStep){
 	// Updates the position
 	position.x += velocity.x*timeStep + 0.5*(cumalForces.x/objectMass)*(timeStep*timeStep);
 	position.x += velocity.y*timeStep + 0.5*(cumalForces.y/objectMass)*(timeStep*timeStep);

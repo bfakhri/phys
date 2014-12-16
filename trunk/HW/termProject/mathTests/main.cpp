9521f5ea85d43bedcab4b7b9771d4ca290395bd1
bijan@umich.edu <bijan@umich.edu@65d18b45-c968-b410-25b9-1799288fbc43>
2014-12-16 10:46:45 +0000

    Just added math tests

---
+++ trunk/HW/termProject/mathTests/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+	double num = 1; 
+	/*
+	for(int i=0; i<608; i++)
+	{
+		num *= 10; 
+		cout << "num:\t" << num << "\ti:\t " << i << endl;
+	}*/
+	
+	double someDub = 0; 
+	cout << sizeof(someDub) << endl; 
+
+	return 0; 
+}

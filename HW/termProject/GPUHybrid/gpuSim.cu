776fa063873efa5a846e474292ee03bce8e2e316
bijan <bijan@umich.edu>
2014-12-18 07:13:12 +0000

    ceck

--- HW/termProject/GPUHybrid/gpuSim.cu
+++ HW/termProject/GPUHybrid/gpuSim.cu
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <iostream>
 #include <math.h>
+#include <omp.h> 
 //#include "mass.cu"
 
 unsigned int N;
@@ -41,6 +42,7 @@ typedef struct Mass
 	double cumalForcesZ;
 }Mass;
 
+__host__
 __device__
 void resetForces(Mass *m){
 	m->cumalForcesX = 0;
@@ -48,11 +50,13 @@ void resetForces(Mass *m){
 	m->cumalForcesZ = 0;
 }
 
+__host__
 __device__
 double newtonGrav(double m1Mass, double m2Mass, double distance, double localG){
 	return localG*(m1Mass*m2Mass)/(distance*distance);
 }
 
+__host__
 __device__
 void influence(Mass *m1, Mass *m2, double localG){
 	double diffPosX = m1->positionX - m2->positionX;
@@ -68,6 +72,7 @@ void influence(Mass *m1, Mass *m2, double localG){
 	m1->cumalForcesZ += netForce * diffPosZ/distance;
 }
 
+__host__
 __device__
 void updateVelAndPos(Mass *m, double timeStep){
 	double accelerationX = m->cumalForcesX/m->objectMass;
@@ -85,37 +90,83 @@ void updateVelAndPos(Mass *m, double timeStep){
 	m->velocityZ += (accelerationZ)*(timeStep);
 }
 
+__host__
+inline void h_simulateTic(Mass * masses, unsigned long numMasses, double deltaT, Mass proxyMass, double localG)
+{
+	// Calc forces on all masses
+	#pragma omp parallel for schedule(static)
+	for(unsigned long i=0; i<numMasses; i++){ 
+		for(unsigned long j=0; j<numMasses; j++){
+			if(i != j)
+				influence(&masses[i], &masses[j], localG); 
+		}
+		influence(&masses[i], &proxyMass, localG); 
+	}
+
+	// Update position of all masses
+	#pragma omp parallel for schedule(static)
+	for(unsigned int i=0; i<numMasses; i++){
+		updateVelAndPos(&masses[i], deltaT); 
+		resetForces(&masses[i]);
+	}
+	 
+}
 __global__
-void simulate(Mass * masses, unsigned long numMasses, unsigned int massesPerBlock, double deltaT, unsigned long totalTimeSteps, double localG)
+void d_simulateTic(Mass * masses, unsigned long numMasses, unsigned int massesPerBlock, double deltaT, Mass * proxyMass, double localG)
 {
 	unsigned int myId = blockIdx.x*massesPerBlock + threadIdx.x; 
-	for(unsigned int i=0; i<totalTimeSteps; i++)
-	{
-		// Sync threads so positions are not updated before all other 
-		__syncthreads(); 
+	// Calc forces on all masses
+	for(unsigned long i=0; i<numMasses; i++){
+		if(i != myId)
+			influence(&masses[myId], &masses[i], localG); 
+	}
+	influence(&masses[myId], proxyMass, localG); 
 
-		// Calc forces on all masses
-		for(unsigned long i=0; i<numMasses; i++){
-			if(i != myId)
-				influence(&masses[myId], &masses[i], localG); 
-		}
-		
+	// Sync threads so positions are not updated before all other 
+	__syncthreads(); 
 
-		// Sync threads so positions are not updated before all other 
-		__syncthreads(); 
+	// Update position of all masses
+	updateVelAndPos(&masses[myId], deltaT); 
 
-		// Update position of all masses
-		updateVelAndPos(&masses[myId], deltaT); 
-
-		// Reset forces
-		resetForces(&masses[myId]);
-	} 
+	// Reset forces
+	resetForces(&masses[myId]);
+	 
 }
 
 __global__
-void testEff(Mass * masses)//, unsigned long numMasses, double deltaT, unsigned long totalTimeSteps, double localG)
+void d_computeProxyMass(Mass * masses, Mass * proxyMass, unsigned int numMasses)
 {
-	masses[threadIdx.x].positionX = (double)threadIdx.x; 
+	proxyMass->objectMass = 0; 
+	proxyMass->positionX = 0; 
+	proxyMass->positionY = 0; 
+	proxyMass->positionZ = 0;
+	for(unsigned int i=0; i<numMasses; i++){
+		proxyMass->objectMass += masses[i].objectMass; 
+		proxyMass->positionX += masses[i].positionX*masses[i].objectMass; 
+		proxyMass->positionY += masses[i].positionY*masses[i].objectMass; 
+		proxyMass->positionZ += masses[i].positionZ*masses[i].objectMass; 
+	}
+	proxyMass->positionX /= proxyMass->objectMass; 
+	proxyMass->positionY /= proxyMass->objectMass; 
+	proxyMass->positionZ /= proxyMass->objectMass; 
+}
+__host__
+void h_computeProxyMass(Mass * masses, Mass * proxyMass, unsigned int numMasses)
+{
+	proxyMass->objectMass = 0; 
+	proxyMass->positionX = 0; 
+	proxyMass->positionY = 0; 
+	proxyMass->positionZ = 0;
+	#pragma omp parallel for schedule(static)
+	for(unsigned int i=0; i<numMasses; i++){
+		proxyMass->objectMass += masses[i].objectMass; 
+		proxyMass->positionX += masses[i].positionX*masses[i].objectMass; 
+		proxyMass->positionY += masses[i].positionY*masses[i].objectMass; 
+		proxyMass->positionZ += masses[i].positionZ*masses[i].objectMass; 
+	}
+	proxyMass->positionX /= proxyMass->objectMass; 
+	proxyMass->positionY /= proxyMass->objectMass; 
+	proxyMass->positionZ /= proxyMass->objectMass; 
 }
 
 __global__
@@ -138,31 +189,44 @@ int main(int argc, char ** argv)
 	double TIME_STEP_SIZE = 1;
 	unsigned long TOTAL_SIM_STEPS = 1000;
 
+	unsigned int L = 1536;//512;//N/2; 	// Number of masses on CPU
+	unsigned int M = 512;//N/2;	// Number of masses on GPU 
 	// Custom simulation parameters 
 	if(argc > 1)
 	{
-		if(argc != 5){
+		if(argc != 6){
 			std::cout << std::endl << "ERROR, incorrect number of arguments" << std::endl; 
 			return -1; 
 		}else{
 			TOTAL_SIM_STEPS = atoi(argv[1]); 
 			TIME_STEP_SIZE = atoi(argv[2]); 
 			N = atoi(argv[3]); 
-			MASSES_PER_CORE = atoi(argv[4]); 
+			//MASSES_PER_CORE = atoi(argv[4]); 
+			L = atoi(argv[4]);
+			M = atoi(argv[5]); 
 		}
+	}else
+	{
+		TOTAL_SIM_STEPS = 100; 
+		TIME_STEP_SIZE = 10000; 
+		N = 256; 
+		MASSES_PER_CORE = 1; 
+
 	}
 	std::cout << "Simulation: " << std::endl << "Number of steps = " << TOTAL_SIM_STEPS 
 		<< std::endl << "Size of time step (seconds) = " << TIME_STEP_SIZE
 		<< std::endl << "N (number of masses) = " << N
 		<< std::endl << "Masses per core = " << MASSES_PER_CORE 
 		<< std::endl << "Value of G = " << G << std::endl;
+
  
 
 	// Make masses	
-	Mass * h_massArray =(Mass*) malloc(N*sizeof(Mass)); 
+	Mass * h_massArray =(Mass*) malloc(L*sizeof(Mass)); 
+	Mass * tmp_massArray =(Mass*) malloc(M*sizeof(Mass)); 
 	
-	// Populate array of masses
-	for(int i=0; i<N; i++){
+	// Populate host array of masses
+	for(int i=0; i<L; i++){
 		h_massArray[i].objectMass = scientificNotation(6+i, 23); 
 		h_massArray[i].positionX = scientificNotation(6+i, 10); 	
 		h_massArray[i].positionY = scientificNotation(6+i, 10); 	
@@ -175,100 +239,88 @@ int main(int argc, char ** argv)
 		h_massArray[i].cumalForcesZ = 0; 	
 	}
 
+	for(int i=L; i<(M+L); i++){
+		tmp_massArray[i-L].objectMass = scientificNotation(6+i, 23); 
+		tmp_massArray[i-L].positionX = scientificNotation(6+i, 10); 	
+		tmp_massArray[i-L].positionY = scientificNotation(6+i, 10); 	
+		tmp_massArray[i-L].positionZ = scientificNotation(6+i, 10); 	
+		tmp_massArray[i-L].velocityX = scientificNotation(6+i, 1); 	
+		tmp_massArray[i-L].velocityY = scientificNotation(6+i, 2); 	
+		tmp_massArray[i-L].velocityZ = scientificNotation(6+i, 1); 	
+		tmp_massArray[i-L].cumalForcesX = 0; 	
+		tmp_massArray[i-L].cumalForcesY = 0; 	
+		tmp_massArray[i-L].cumalForcesZ = 0; 	
+	}
+
 	// Start output
 	std::cout << "Start Posisions (X): " << std::endl;
-	//for(int i=0; i<N; i++){
-	//	std::cout << h_massArray[i].positionX << std::endl; 
-	//}
 	std::cout << h_massArray[0].positionX << std::endl; 
+	std::cout << tmp_massArray[0].positionX << std::endl;
 
 
 	// Allocate memory on device for masses
+	std::cout << "M: " << M << std::endl;
 	Mass * d_massArray;
-	cudaMalloc( (void**)&d_massArray, N*sizeof(Mass));
-
-
+	cudaMalloc( (void**)&d_massArray, M*sizeof(Mass));
+	//Mass * d_massArray;
+	//cudaMalloc( (void**)&d_massArray, N*sizeof(Mass));
+	
+	Mass * d_proxyMassCPU;
+	cudaMalloc( (void**)&d_proxyMassCPU, sizeof(Mass));
+	Mass * d_proxyMassGPU;
+	cudaMalloc( (void**)&d_proxyMassGPU, sizeof(Mass));
+	
 	// Copy masses onto device
-	cudaMemcpy( d_massArray, h_massArray, (N*sizeof(Mass)), cudaMemcpyHostToDevice );
+	cudaMemcpy( d_massArray, tmp_massArray, (M*sizeof(Mass)), cudaMemcpyHostToDevice );
+
+	// Free local memory for temp array
+	free(tmp_massArray); 
 
 	// Dimensions for cuda function call 
 	dim3 blockDimensions( 32, 1 );
 	dim3 gridDimensions( 8, 1 );
+	dim3 blockSingle(1, 1); 
+	dim3 gridSingle(1, 1); 	
 
 	// Do simi
-	unsigned int massesPerBlock = N/blockDimensions.x; 
-	//unsigned int massesPerThread = massesPerBlock/gridDimensions.x;
+	unsigned int massesPerBlock = M/blockDimensions.x; 
 
 
 	// Split data between CPU and GPU
+	// Masses 0-(dividor-1) go to CPU
+	// Masses dividor-(N-1) go to GPU	
 	
-	
-	for(unsigned int t=0; t<TOTAL_TIME_STEPS; t++)
+	for(unsigned int t=0; t<TOTAL_SIM_STEPS; t++)
 	{
-		// Make proxy mass for both clumps
-		Mass proxyMassCPU;
-		proxyMassCPU.objectMass = 0; 
-		proxyMassCPU.positionX = 0; 
-		proxyMassCPU.positionY = 0; 
-		proxyMassCPU.positionZ = 0; 
-		for(unsigned int d=0; d<dividor; d++)
-		{
-			proxyMassCPU.objectMass += h_massArray[d].objectMass;
-			proxyMassCPU.positionX += h_massArray[d].positionX;
-			proxyMassCPU.positionY += h_massArray[d].positionY;
-			proxyMassCPU.positionZ += h_massArray[d].positionZ;
-		}
-		// Calculate averages
-		proxyMassCPU.objectMass /= dividor;
-		proxyMassCPU.positionX /= dividor;
-		proxyMassCPU.positionY /= dividor;
-		proxyMassCPU.positionZ /= dividor;
+		// Make proxy mass for GPU masses
+		d_computeProxyMass<<< gridSingle, blockSingle >>>(d_massArray, d_proxyMassGPU, L); 
+
+		// Make proxy mass for CPU masses
+		Mass h_proxyMassCPU;
+		h_computeProxyMass(h_massArray, &h_proxyMassCPU, L); 
+	
+		// Send CPU proxy mass to GPU
+		cudaMemcpy( d_proxyMassCPU, &h_proxyMassCPU, sizeof(Mass), cudaMemcpyHostToDevice );
 		
-			
-		Mass proxyMassGPU;
-		proxyMassGPU.objectMass = 0; 
-		proxyMassGPU.positionX = 0; 
-		proxyMassGPU.positionY = 0; 
-		proxyMassGPU.positionZ = 0; 
-		for(unsigned int d=0; d<N-dividor; d++)
-		{
-			proxyMassGPU.objectMass += h_massArray[d+dividor].objectMass;
-			proxyMassGPU.positionX += h_massArray[d+dividor].positionX;
-			proxyMassGPU.positionY += h_massArray[d+dividor].positionY;
-			proxyMassGPU.positionZ += h_massArray[d+dividor].positionZ;
-		}
-		// Calculate averages
-		proxyMassGPU.objectMass /= (N-dividor);
-		proxyMassGPU.positionX /= (N-dividor);
-		proxyMassGPU.positionY /= (N-dividor);
-		proxyMassGPU.positionZ /= (N-dividor);
-		
-		// Send GPU its portion of the array and proxy mass
-
-		// Perform one tick of simulation
-
-
+		// Receive proxy mass from GPU 
+		Mass h_proxyMassGPU;
+		cudaMemcpy( &h_proxyMassGPU, d_proxyMassGPU, sizeof(Mass), cudaMemcpyDeviceToHost );
+	
+		// Perform one tick of simulation on CPU and GPU 
+		// CALL GPU FUNCT
+		d_simulateTic <<< gridDimensions, blockDimensions >>>(d_massArray, M, massesPerBlock, TIME_STEP_SIZE, d_proxyMassCPU, G); 
+		// CALL CPU FUNCT
+		h_simulateTic(h_massArray, L, TIME_STEP_SIZE, h_proxyMassGPU, G); 
 	}
  
-	simulate<<< gridDimensions, blockDimensions >>>(d_massArray, N, massesPerBlock, TIME_STEP_SIZE, TOTAL_SIM_STEPS, G);
-	//simulate<<< gridDimensions, blockDimensions >>>(d_massArray, N, MASSES_PER_CORE, TIME_STEP_SIZE, TOTAL_SIM_STEPS, G);
-	//testEff<<< gridDimensions, blockDimensions >>>(d_massArray);
-	//testInfluence<<< gridDimensions, blockDimensions >>>(d_massArray, N, G, d_dist); 
 
-	// Get data back
-	cudaMemcpy( h_massArray, d_massArray, (N*sizeof(Mass)), cudaMemcpyDeviceToHost );
-
-	// Free device mem 
-	cudaFree( d_massArray );
+	tmp_massArray =(Mass*) malloc(M*sizeof(Mass)); 
+	cudaMemcpy( tmp_massArray, d_massArray, (M*sizeof(Mass)), cudaMemcpyDeviceToHost );
 
 	// Output
 	std::cout << std::endl << "End Posisions (X): " << std::endl;
-	//std::cout << "Distance: " << h_dist << std::endl;
-	//for(int i=0; i<N; i++){
-	//	std::cout << h_massArray2[i].positionX << std::endl; 
-	//	std::cout << h_massArray2[i].cumalForcesX << std::endl; 	
-	//}
 	std::cout << h_massArray[0].positionX << std::endl; 
-
+	std::cout << tmp_massArray[0].positionX << std::endl;
+	
 	return EXIT_SUCCESS;
 }

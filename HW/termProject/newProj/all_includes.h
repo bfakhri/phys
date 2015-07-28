dfe30e53cf1a0cb8df4a24d659bb09a79a5ac44e
draco <bfakhri@asu.edu>
2015-07-28 05:22:33 -0400

    Deleted old versions

--- HW/termProject/newProj/all_includes.h
+++
@@ -1,8 +0,0 @@
-// Contains references to all of the global variables and constants etc
-
-// Defines the cartesian structure
-typedef struct{
-	double x; 
-	double y;
-	double z; 
-}cart;
--- HW/termProject/newProj/draw.h
+++
@@ -1,7 +0,0 @@
-#ifndef DRAWSHAPES_H
-#define DRAWSHAPES_H
-#include "all_includes.h"
-
-void drawAllShapes(); 
-
-#endif 

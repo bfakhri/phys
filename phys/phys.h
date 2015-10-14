c8d49e706ecb51449c6560333f8c9ce8497ea9f6
kepler <bfakhri@asu.edu>
2015-10-14 05:11:01 -0400

    Set better physics params

--- phys/phys.h
+++ phys/phys.h
@@ -22,8 +22,8 @@
 
 // Simulation parameters
 // Default period for each step (seconds)
-const double SIM_FPS = 100;						// FPS of the physics engine
-const double SIM_T = 0.0001;						// Default sim period
+const double SIM_FPS = 200;						// FPS of the physics engine
+const double SIM_T = 0.005;						// Default sim period
 const double G_CONST = 0.0000000000667384;		// Gravitational constant G
 const double GRAV_ACCEL = 9.81;					// Accel due to gravity in m/s^2
 const double SPRING_CONST = 50;				// Need to tweak this

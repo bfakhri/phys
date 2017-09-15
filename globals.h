f791247d8f4878914ffc1f0ca1ee587c96da4578
tubi <bfakhri@asu.edu>
2017-09-15 09:50:24 -0700

    update

--- globals.h
+++ globals.h
@@ -2,7 +2,9 @@
 #ifndef GLOBALS_H
 #define GLOBALS_H
 
-#define SCREEN_RES_X 1000
-#define SCREEN_RES_Y 1000
+//#define SCREEN_RES_X 1000
+//#define SCREEN_RES_Y 1000
+#define SCREEN_RES_X 1440 
+#define SCREEN_RES_Y 900
 
 #endif
